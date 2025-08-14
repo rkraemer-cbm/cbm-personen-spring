6a7c5ce92870cebda6543f01de148d580b91e56d
Ronny Krämer <ronny.kraemer@uni-bremen.de>
2025-08-14 10:01:21 +0200

    add onDocumentReady

--- src/main/resources/static/js/scanner.js
+++ src/main/resources/static/js/scanner.js
@@ -1,30 +1,30 @@
 $(document).ready(() => {
-const config = {
-    inputStream: {
-        name: "Live",
-        type: "LiveStream",
-        target: document.querySelector('#interactive'),
-        constraints: {
-            width: 640,
-            height: 480,
-            facingMode: "environment"
+    const config = {
+        inputStream: {
+            name: "Live",
+            type: "LiveStream",
+            target: document.querySelector('#interactive'),
+            constraints: {
+                width: 640,
+                height: 480,
+                facingMode: "environment"
+            }
+        },
+        decoder: {
+            readers: ['ean_reader', 'code_128_reader']
         }
-    },
-    decoder: {
-        readers: ['ean_reader', 'code_128_reader']
-    }
-};
+    };
 
-Quagga.init(config, err => {
-    if (err) {
-        console.log(err);
-        $('#code').text('Fehler beim Initialisieren ' + err);
-        return;
-    }
-    Quagga.start();
-});
+    Quagga.init(config, err => {
+        if (err) {
+            console.log(err);
+            $('#code').text('Fehler beim Initialisieren ' + err);
+            return;
+        }
+        Quagga.start();
+    });
 
-Quagga.onDetected(data => {
-    $('#code').val(data.codeResult.code);
-})
+    Quagga.onDetected(data => {
+        $('#code').val(data.codeResult.code);
+    })
 })
